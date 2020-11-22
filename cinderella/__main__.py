b99a3c5c922a0b11f610f8d70762a4582d8b1be1
dangeroftelegram <67426127+dangeroftelegram@users.noreply.github.com>
2020-11-22 09:33:27 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -179,7 +179,7 @@ def send_start(bot, update):
     text = PM_START_TEXT
 
     keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="❣️Creator❣️",url="https://t.me/Danger_of_telegram")]]
-    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text=⚡️Add Me⚡️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚡️Add Me⚡️",url="t.me/{}?startgroup=true".format(bot.username))]]
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
