2e567ce3064587b8732b5879256c6ff64241ad2c
dangeroftelegram <67426127+dangeroftelegram@users.noreply.github.com>
2020-11-22 11:42:49 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -58,7 +58,7 @@ def vercheck() -> str:
 
 SOURCE_STRING = """
 ⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/dangeroftelegram/Queen_pro)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template= https://github.com/dangeroftelegram/Queen_pro)
+⚡You Can Clone Me [Here](https://heroku.com/deploy?template= https://github.com/dangeroftelegram/Queen_pro.git)
 """
 
 
